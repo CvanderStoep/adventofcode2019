a1fca7c2fcb33190841e77348f9acf59630f4a22
Carlo van der Stoep <carlo.vanderstoep@gmail.com>
2024-08-20 10:28:59 +0200

    day 1 completed

--- day1.py
+++ day1.py
@@ -2,20 +2,38 @@ def read_input_file(file_name: str) -> list:
     with open(file_name) as f:
         content = f.read().splitlines()
 
-    content = list(map(int, content))
+    content = [int(c) for c in content]
 
     return content
 
 
 def compute_part_one(file_name: str) -> int:
     inputs = read_input_file(file_name)
-    print(inputs)
-    return 0
+    sum_mass = 0
+    for mass in inputs:
+        mass = mass // 3 - 2
+        sum_mass += mass
+
+    a = sum([m//3-2 for m in inputs])
+    b = [(lambda f: f//3 - 2)(m) for m in inputs]
+    print(f'{sum(b)= }')
+    print(f'{a= }')
+
+    return sum_mass
 
 
 def compute_part_two(file_name: str) -> int:
     inputs = read_input_file(file_name)
-    return 1
+    sum_mass = 0
+    for mass in inputs:
+        mass = mass // 3 - 2
+        while mass > 0:
+            sum_mass += mass
+            mass = mass // 3 - 2
+
+    return sum_mass
+
+
 
 
 if __name__ == '__main__':
